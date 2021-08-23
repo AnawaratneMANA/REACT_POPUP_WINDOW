0848acad0d0cc2327486b540835e249018c44a1d
Administrator <admin@example.com>
2021-08-23 21:39:59 +0530

    final commitment to the popup window react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Popup from "./components/Popup";
+import {useState, useEffect} from "react"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [timedPopup, setTimedPopup] = useState(false);
+
+    useEffect(() => {
+        setTimeout(()=> {
+            setTimedPopup(true);
+        }, 3000);
+    }, [])
+
+
+    return (
+        <div className="App">
+            <main>
+                <h1> React Popup</h1>
+                <br/>
+                <br/>
+                <button onClick={() => setButtonPopup(true)}>Open Popup Menu</button>
+                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                    <h3>My Popup</h3>
+                    <p>This is my button triggered Form Popup.</p>
+                    <p>Add the form here.</p>
+                </Popup>
+
+                <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+                    <h3>My Popup</h3>
+                    <p>This is my button triggered Form Popup.</p>
+                    <p>Add the form here.</p>
+                </Popup>
+            </main>
+        </div>
+    );
 }
 
 export default App;
